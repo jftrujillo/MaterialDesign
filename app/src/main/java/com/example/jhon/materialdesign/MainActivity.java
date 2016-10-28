0ac67b8c4b5f42e3c7465713aaf80b05dd68f074
Jhon <jftrujillo@unicauca.edu.co>
2016-10-28 07:56:07 -0500

    agrega logica al actiivty para recuperar elementos

--- app/src/main/java/com/example/jhon/materialdesign/MainActivity.java
+++ app/src/main/java/com/example/jhon/materialdesign/MainActivity.java
@@ -1,17 +1,29 @@
 package com.example.jhon.materialdesign;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.jhon.materialdesign.Util.ValidateStrings;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Toolbar toolbar;
+    Button btn;
+    TextInputLayout email;
+    TextInputLayout pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        btn = (Button) findViewById(R.id.btn);
+        email = (TextInputLayout) findViewById(R.id.email);
+        pass = (TextInputLayout) findViewById(R.id.pass);
+        btn.setOnClickListener(this);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Bienvenido");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -26,4 +38,16 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn:
+                if (ValidateStrings.areStringsValidate( ))
+                break;
+
+        }
+    }
+
+
 }
