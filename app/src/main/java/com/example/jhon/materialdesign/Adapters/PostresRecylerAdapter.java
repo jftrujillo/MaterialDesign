98b20df7e084d595065464d06276d9c619266aaa
Jhon <jftrujillo@unicauca.edu.co>
2016-11-04 07:20:50 -0500

    Termina recycler view

--- app/src/main/java/com/example/jhon/materialdesign/Adapters/PostresRecylerAdapter.java
+++ app/src/main/java/com/example/jhon/materialdesign/Adapters/PostresRecylerAdapter.java
@@ -2,9 +2,15 @@ package com.example.jhon.materialdesign.Adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.jhon.materialdesign.R;
 import com.example.jhon.materialdesign.model.Postre;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -23,16 +29,37 @@ public class PostresRecylerAdapter extends RecyclerView.Adapter<RecyclerView.Vie
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.recycler_postres_adapter,parent,false);
+        Viewholder viewholder = new Viewholder(v);
+        return viewholder;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        Viewholder viewholder = (Viewholder) holder;
+        viewholder.nombre.setText(data.get(position).getNomnbre());
+        viewholder.ingredientes.setText(data.get(position).getIngredientes());
+        viewholder.precio.setText(data.get(position).getPrecion());
+        Picasso.with(context).load(data.get(position).getImgUrl()).into(viewholder.imagen);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
+
+    public static class Viewholder extends RecyclerView.ViewHolder{
+        ImageView imagen;
+        TextView nombre, ingredientes,precio;
+
+        public Viewholder(View itemView) {
+            super(itemView);
+            imagen = (ImageView) itemView.findViewById(R.id.imagen);
+            nombre = (TextView) itemView.findViewById(R.id.nombre);
+            precio = (TextView) itemView.findViewById(R.id.precio);
+            ingredientes = (TextView) itemView.findViewById(R.id.ingredientes);
+        }
+    }
+
 }
--- app/src/main/java/com/example/jhon/materialdesign/RecyclerView.java
+++ app/src/main/java/com/example/jhon/materialdesign/RecyclerView.java
@@ -2,11 +2,17 @@ package com.example.jhon.materialdesign;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
+
+import com.example.jhon.materialdesign.Adapters.PostresRecylerAdapter;
+import com.example.jhon.materialdesign.model.Postre;
 
 public class RecyclerView extends AppCompatActivity {
     Toolbar toolbar;
     android.support.v7.widget.RecyclerView recyclerView;
+    android.support.v7.widget.RecyclerView.Adapter adapter;
+    android.support.v7.widget.RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +21,9 @@ public class RecyclerView extends AppCompatActivity {
         recyclerView = (android.support.v7.widget.RecyclerView) findViewById(R.id.recycler_view);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("RecyclerView");
-
+        adapter = new PostresRecylerAdapter(Postre.initPostres(),this);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
     }
 }
