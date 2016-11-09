27d2c533355130d8ac957a62c0cd255afc42b051
Jhon <jftrujillo@unicauca.edu.co>
2016-11-09 07:32:03 -0500

    Last commit

--- app/src/main/java/com/example/jhon/materialdesign/RecyclerView.java
+++ app/src/main/java/com/example/jhon/materialdesign/RecyclerView.java
@@ -1,5 +1,7 @@
 package com.example.jhon.materialdesign;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,14 +16,21 @@ public class RecyclerView extends AppCompatActivity {
     android.support.v7.widget.RecyclerView recyclerView;
     android.support.v7.widget.RecyclerView.Adapter adapter;
     android.support.v7.widget.RecyclerView.LayoutManager layoutManager;
+    CollapsingToolbarLayout colapse;
+    AppBarLayout appBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        colapse = (CollapsingToolbarLayout) findViewById(R.id.colapse);
+        colapse.setExpandedTitleColor(getResources().getColor(android.R.color.black));
+        colapse.setCollapsedTitleTextColor(getResources().getColor(android.R.color.white));
+        appBar = (AppBarLayout) findViewById(R.id.app_bar_layout);
         recyclerView = (android.support.v7.widget.RecyclerView) findViewById(R.id.recycler_view);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("RecyclerView");
+
+        colapse.setTitle("RecyclerView");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         adapter = new PostresRecylerAdapter(Postre.initPostres(),this);
         layoutManager = new LinearLayoutManager(this);
